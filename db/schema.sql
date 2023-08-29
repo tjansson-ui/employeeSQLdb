4f526e85ad46c85d1d91e5ff48a2b5cc42c079ca
tjansson-ui <59398067+tjansson-ui@users.noreply.github.com>
2023-08-28 19:40:09 -0500

    troubled some schema issues and added js

--- db/schema.sql
+++ db/schema.sql
@@ -3,32 +3,31 @@ CREATE DATABASE company_employee_db;
 
 USE company_employee_db;
 
---Add department table --
-CREATE TABLE department (
-
-    id INT AUTO_INCREMENT,
+--Add department table
+CREATE TABLE departments (
+    id INTEGER AUTO_INCREMENT,
     name VARCHAR(30), 
     PRIMARY KEY (id)
 );
 
---Add role table --
-CREATE TABLE role (
-    id INT AUTO_INCREMENT,
+--Add role table
+CREATE TABLE roles (
+    id INTEGER AUTO_INCREMENT,
     title VARCHAR(30), --to hold role title
     salary DECIMAL, --to hold role salary
-    department_id INT, --to hold reference to department role belongs to
+    department_id INTEGER, --to hold reference to department role belongs to
     PRIMARY KEY (id),
-    FOREIGN KEY (department_id) REFERENCES department(id)
+    FOREIGN KEY (department_id) REFERENCES departments(id)
 );
 
---Add employee table --
-CREATE TABLE employee (
-    id INT AUTO_INCREMENT,
+--Add employee table
+CREATE TABLE employees (
+    id INTEGER AUTO_INCREMENT,
     first_name VARCHAR(30), --to hold employee first name
     last_name VARCHAR(30), --to hold employee last name
-    role_id INT, --to hold reference to employee role
-    manager_id INT DEFAULT NULL,-- to hold reference to another employee that is the manager of the current employee 
+    role_id INTEGER, --to hold reference to employee role
+    manager_id INTEGER DEFAULT NULL,-- to hold reference to another employee that is the manager of the current employee 
     PRIMARY KEY (id),
-    FOREIGN KEY (role_id) REFERENCES role(id),
-    FOREIGN KEY (manager_id) REFERENCES employee(id)
-    ); 
+    FOREIGN KEY (role_id) REFERENCES roles(id),
+    FOREIGN KEY (manager_id) REFERENCES employees(id)
+); 
