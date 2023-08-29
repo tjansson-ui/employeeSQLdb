44fd9dc835d9561037d3f9ffd6ececcb27ff73bb
tjansson-ui <59398067+tjansson-ui@users.noreply.github.com>
2023-08-28 19:12:49 -0500

    adding seeds and more switch cases

--- db/schema.sql
+++ db/schema.sql
@@ -6,26 +6,29 @@ USE company_employee_db;
 --Add department table --
 CREATE TABLE department (
 
-    id INTEGER AUTO_INCREMENT,
+    id INT AUTO_INCREMENT,
     name VARCHAR(30), 
     PRIMARY KEY (id)
 );
 
 --Add role table --
 CREATE TABLE role (
-    id INTEGER AUTO_INCREMENT,
+    id INT AUTO_INCREMENT,
     title VARCHAR(30), --to hold role title
     salary DECIMAL, --to hold role salary
     department_id INT, --to hold reference to department role belongs to
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 --Add employee table --
 CREATE TABLE employee (
-    id INTEGER AUTO_INCREMENT,
+    id INT AUTO_INCREMENT,
     first_name VARCHAR(30), --to hold employee first name
     last_name VARCHAR(30), --to hold employee last name
-    role_id INTEGER, --to hold reference to employee role
-    manager_id INTEGER DEFAULT NULL,-- to hold reference to another employee that is the manager of the current employee 
+    role_id INT, --to hold reference to employee role
+    manager_id INT DEFAULT NULL,-- to hold reference to another employee that is the manager of the current employee 
     PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
     ); 
---
+++ db/seeds.sql
@@ -0,0 +1,34 @@
+INSERT INTO departments (department_name) VALUES
+("Operations"),
+("Research"),
+("Marketing"),
+("Sales"),
+("IT"),
+("Finance"),
+("HR");
+
+INSERT INTO roles (title, salary, department_id) VALUES
+("Production Associate", 55550.00, 1),
+("Scientist", 64980.00, 2),
+("Research Supervisor", 75000.00, 2),
+("Sales Manager", 92000.00, 4),
+("Sales Associate", 44680.00, 4),
+("Tech Support Analyst", 63800.00, 5),
+("HR Business Partner", 68880.00, 7),
+("VP of Customer Outreach", 195880.00, 4),
+("Chief Finance Officer", 275640.00, 6),
+("Marketing Director", 152010.00, 3);
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES
+("Bob", "Igler", 9, null),
+("Beff", "Jezos", 8, 1),
+("Frank", "Enstein", 2, 4),
+("Rhapsody", "Blue", 3, 1),
+("Thomas", "Foolery", 5, 6),
+("Nicole", "Tesle", 6, 2),
+("Space", "Ghost", 10, 3),
+("Minion", "Despic", 1, 3),
+("Chester", "Cheetoh", 1, 3),
+("Capin", "Krunz", 1, 3),
+("Coun", "Tchockula", 1, 3),
+("North", "Carolina", 7, 2);
